fcfdc2829f6948e4b6e33e3ce6b4c79f16113486
Dean Silfen <dean.silfen@gmail.com>
2015-12-24 02:11:37 -0500

    Add first spec

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ end
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
+  gem 'byebug'
 end
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe ApiController, type: :controller do
+  describe '#clues' do
+  end
 
+  describe '#random' do
+  end
+
+  describe '#categories' do
+  end
+
+  describe '#single_category' do
+    it 'returns the right category for id' do
+      category = Category.first
+      get :single_category, id: category.id, format: :json
+      expect(response).to be_success
+
+      body = JSON.parse(response.body)
+      expect(body["id"]).to eq(category.id)
+      expect(body["title"]).to eq(category.title)
+      expect(body["clues_count"]).to eq(category.clues_count)
+    end
+  end
+
+  describe '#mark_invalid' do
+  end
 end
+
