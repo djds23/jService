501c210429920c02c86e2d8296446c8c727f2c11
Dean Silfen <dean.silfen@gmail.com>
2015-12-24 23:08:46 -0500

    Add factory_girl & more tests

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe ApiController, type: :controller do
   describe '#clues' do
+    let(:recent_clue) { FactoryGirl.create(:clue, airdate: Time.now - 1.week ) }
+    let(:cheap_clue)  { FactoryGirl.create(:clue, value: 100 ) }
+    let(:new_clue )   { FactoryGirl.create(:clue, airdate: Time.now ) }
+
+    it 'only returns the proper valued clues' do
+      get :clues, value: cheap_clue.value, format: :json
+      expect(response).to be_success
+
+      body = JSON.parse(response.body)
+      expect(body.count).to eq(1)
+
+      clue = body.first
+      expect(clue["id"]).to eq(cheap_clue.id)
+    end
+
+    it 'only returns clues within the proper airdates' do
+      get :clues, min_date: recent_clue.airdate.to_s, max_date: (new_clue.airdate + 1.day).to_s, format: :json
+      expect(response).to be_success
+
+      body = JSON.parse(response.body)
+      expect(body.count).to eq(2)
+
+      response_ids = body.map { |h| h["id"] }
+      expect(response_ids).to include(recent_clue.id)
+      expect(response_ids).to include(new_clue.id)
+    end
+
+    it 'only returns clues from the proper category' do
+      get :clues, value: cheap_clue.value, format: :json
+      expect(response).to be_success
+      byebug
+    end
   end
 
   describe '#random' do
@@ -11,8 +43,8 @@ RSpec.describe ApiController, type: :controller do
   end
 
   describe '#single_category' do
+    let(:category) { FactoryGirl.create(:category) }
     it 'returns the right category for id' do
-      category = Category.first
       get :single_category, id: category.id, format: :json
       expect(response).to be_success
 
