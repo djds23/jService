7c425f3fa05db9f748b467dab9338edcd3d7afd7
Dean Silfen <dean.silfen@gmail.com>
2016-11-04 18:20:49 -0400

    Remove unused views

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 JArchive::Application.routes.draw do
 
-  get "search" => "search#index"
-  get "search/:query" => "search#index", :as => "search_term"
-
-  get "popular" => "popular#index"
-  get "popular/:category_id" => "popular#list", :as => "popular_list"
   get "api/random" => "api#random",  defaults: {format: :json}
   get "api/clues" => "api#clues", defaults: {format: :json}
   get "api/categories" => "api#categories", defaults: {format: :json}
   get "api/category" => "api#single_category", defaults: {format: :json}
   post "api/invalid" => "api#mark_invalid", defaults: {format: :json}
 
-  get "index/home", :as => "home"
   get "index/about"
 
   resources :categories
